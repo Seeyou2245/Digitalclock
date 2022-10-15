a6aa05f6ca99268acd0b2b878b8292509cc7ade0
Seeyou2245 <109306905+Seeyou2245@users.noreply.github.com>
2022-10-15 10:10:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Digitalclock from "./Digitalclock";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Digitalclock />
+    </div>
+  );
+}
